b38ad8932ad5e8e0197316e7d8375642128cdc86
Amit Aharoni <ChefAharoni@proton.me>
2025-08-07 12:45:20 -0400

    * Completed input checking

--- src/ShortestPaths.java
+++ src/ShortestPaths.java
@@ -20,12 +20,17 @@ public class ShortestPaths
     {
         StringBuilder sb = new StringBuilder();
         String line;
+        int lineNum = 1;
+        int vert = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
         {
             while((line = br.readLine()) != null)
             {
                 sb.append(line).append(System.lineSeparator());
+                if (lineNum == 1) vert = checkVertInput(line);
+                else checkInput(line, lineNum, vert);
+                lineNum++;
             }
         } catch (IOException ioe)
         {
@@ -36,8 +41,109 @@ public class ShortestPaths
         return sb.toString();
     }
 
+
+    private static void checkInput(String line, int lineNum, int vertices)
+    {
+        String[] args = line.split(" ");
+        int delta = (int) 'A' + vertices - 1;
+
+        if (args.length != 3)
+        {
+            System.err.println("Error: Invalid edge data '" + line +
+                    "' on line " + lineNum + ".");
+            System.exit(1);
+        }
+
+        int weight = 0;
+        try
+        {
+            weight = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e)
+        {
+            System.err.println("Error: Invalid edge weight '" + args[2] +
+                            "' on line " + lineNum + ".");
+            System.exit(1);
+        }
+
+        if (weight < 1)
+        {
+            System.err.println("Error: Invalid edge weight '" + weight +
+                    "' on line " + lineNum + ".");
+            System.exit(1);
+        }
+
+        // Starting vertex (if longer than char)
+        if (args[0].length() != 1)
+        {
+            System.err.println("Error: Starting vertex '"+ args[0] +
+                    "' on line " + lineNum +  " is not among valid values " +
+                    "A-"+ (char) delta + ".");
+            System.exit(1);
+        }
+
+        // Ending vertex (if longer than char)
+        if (args[1].length() != 1)
+        {
+            System.err.println("Error: Ending vertex '"+ args[1] +
+                    "' on line " + lineNum +  " is not among valid values " +
+                    "A-"+ (char) delta + ".");
+            System.exit(1);
+        }
+
+        // Strip all spaces from the line
+        String cleanLine = line.replaceAll("\\s+", "");
+        char[] charArr = cleanLine.toCharArray();
+
+        // Check that the vertices are in range (A is 65 in ASCII)
+        // Starting vertex
+        int asciiVal = (int) charArr[0];
+        if (asciiVal > delta || asciiVal < 65)
+        {
+            System.err.println("Error: Starting vertex '"+ charArr[0] +
+                    "' on line " + lineNum +  " is not among valid values " +
+                    "A-"+ (char) delta + ".");
+            System.exit(1);
+        }
+
+        // Ending vertex
+        asciiVal = (int) charArr[1];
+        if (asciiVal > delta || asciiVal < 65)
+        {
+            System.err.println("Error: Ending vertex '"+ charArr[1] +
+                    "' on line " + lineNum +  " is not among valid values " +
+                    "A-"+ (char) delta + ".");
+            System.exit(1);
+        }
+    }
+
+    private static int checkVertInput(String firstLine)
+    {
+        int vertNum = 0;
+
+        try
+        {
+            vertNum = Integer.parseInt(firstLine);
+        } catch (NumberFormatException e)
+        {
+            // TODO: verify this is the correct error message
+            System.err.println("Error: Invalid number of vertices '"
+                    + firstLine + "' on line 1.");
+            System.exit(1);
+        }
+
+        if (vertNum < 1 || vertNum > 26)
+        {
+            System.err.println("Error: Invalid number of vertices '"
+                    + firstLine + "' on line 1.");
+            System.exit(1);
+        }
+
+        return vertNum;
+    }
+
+
     public static void testMethod(String file)
     {
-        System.out.println("Testing " + file);
+        System.out.println("File contents:\n" + file);
     }
 }
